c80c5a570193597863e26a64adc74b0bcacc5837
jwilliams493 <60114121+jwilliams493@users.noreply.github.com>
2023-03-21 10:09:52 -0400

    Verifying Git CLI usage

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     __FEATURE_OPTIONS__: true,
     __FEATURE_SUSPENSE__: true
   },
+//Test
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'lcov', 'text'],
   collectCoverageFrom: [
